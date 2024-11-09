c5fd4d63ed82a67ef1c807ae749f242e2cd61f9c
netbrum <netbrum@tutanota.com>
2024-11-09 20:30:33 +0100

    feat: add vga text buffer interface

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 #![no_std]
 #![no_main]
 
-static HELLO: &[u8] = b"Hello World!";
+use vga::WRITER;
+
+mod vga;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xF;
-        }
+    for i in 0..=50 {
+        println!("{}", i);
     }
 
-    panic!()
+    loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    WRITER.lock().reset();
+    println!("{}", info);
+
     loop {}
 }
---
+++ src/vga.rs
@@ -0,0 +1,165 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+enum Color {
+    Black = 0,
+    BLue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> Self {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct BufferChar {
+    character: u8,
+    color: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[volatile::Volatile<BufferChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+const VGA_START_ADDRESS: usize = 0xb8000;
+
+pub struct Writer {
+    column: usize,
+    row: usize,
+    color: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Default for Writer {
+    fn default() -> Self {
+        Writer {
+            column: 0,
+            row: 1,
+            color: ColorCode::new(Color::White, Color::Black),
+            buffer: unsafe { &mut *(VGA_START_ADDRESS as *mut Buffer) },
+        }
+    }
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.newline(),
+            byte => {
+                if self.column >= BUFFER_WIDTH {
+                    self.newline();
+                }
+
+                let col = self.column;
+                let row = self.row;
+
+                self.buffer.chars[row][col].write(BufferChar {
+                    character: byte,
+                    color: self.color,
+                });
+
+                self.column += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, string: &str) {
+        for byte in string.bytes() {
+            match byte {
+                0x20..0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn newline(&mut self) {
+        if self.row == BUFFER_HEIGHT - 1 {
+            self.clear_row(0);
+
+            for row in 1..BUFFER_HEIGHT {
+                for col in 0..BUFFER_WIDTH {
+                    let character = self.buffer.chars[row][col].read();
+                    self.buffer.chars[row - 1][col].write(character);
+                }
+            }
+        } else {
+            self.row += 1;
+        }
+
+        self.column = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = BufferChar {
+            character: b' ',
+            color: self.color,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.column = 0;
+        self.row = 1;
+
+        for row in 0..BUFFER_HEIGHT {
+            self.clear_row(row);
+        }
+    }
+}
+
+impl core::fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer::default());
+}
+
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
